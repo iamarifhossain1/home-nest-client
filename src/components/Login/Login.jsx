ca9b39a0325049cf0c705d921a252cfed76fe3ba
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-12-03 23:38:11 +0600

    login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { use } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
+import { useLocation, useNavigate } from 'react-router';
 
 const Login = () => {
+
+    const { signInUser, user } = use(AuthContext)
+
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        e.target.reset();
+        signInUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                navigate(`${location.state ? location.state : '/'}`)
+
+            })
+    }
+
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                     <div className="card-body">
                         <h1 className="text-5xl font-bold">Login now!</h1>
-                        <fieldset className="fieldset">
-                            <label className="label">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
-                            <label className="label">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
-                            <div><a className="link link-hover">Forgot password?</a></div>
-                            <button className="btn btn-neutral mt-4">Login</button>
-                        </fieldset>
+                        <form onSubmit={handleLogin}>
+                            <fieldset className="fieldset">
+                                <label className="label">Email</label>
+                                <input type="email" name='email' className="input" placeholder="Email" />
+                                <label className="label">Password</label>
+                                <input type="password" name='password' className="input" placeholder="Password" />
+                                <div><a className="link link-hover">Forgot password?</a></div>
+                                <button className="btn btn-neutral mt-4">Login</button>
+                            </fieldset>
+                        </form>
                     </div>
                 </div>
             </div>
